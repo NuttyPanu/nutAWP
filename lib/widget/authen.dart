c41c8213dd579b0fbf4688c777783d699650a2e1
nuttypanu <nuttypanu@gmail.com>
2020-09-21 18:55:29 +0700

    finnish240963

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nuttypwa/widget/register.dart';
 
 class Authen extends StatefulWidget {
   Authen({Key key}) : super(key: key);
@@ -8,26 +10,113 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            buildContainer(),
-            buildText(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment(0, -0.4),
+            radius: 1.0,
+            colors: [Colors.white, Colors.lime],
+          ),
+        ),
+        // decoration: BoxDecoration(color: Colors.lime),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                buildContainer(),
+                buildText(),
+                buildContainerUser(),
+                buildContainerPassword(),
+                buildLogin(),
+                buildFlatButton()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  FlatButton buildFlatButton() {
+    return FlatButton(
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Register(),
+            ));
+      },
+      child: Text(
+        'New Register',
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  Container buildLogin() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: RaisedButton(
+        color: Colors.purple.shade700,
+        onPressed: () {},
+        child: Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Container buildContainerUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'User :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container buildContainerPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: TextField(
+        obscureText: statusRedEye,
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          border: OutlineInputBorder(),
+          suffixIcon: IconButton(
+              icon: Icon(Icons.remove_red_eye),
+              onPressed: () {
+                setState(() {
+                  statusRedEye = !statusRedEye;
+                });
+              }),
         ),
       ),
     );
   }
 
   Text buildText() => Text(
-        'Nut PWA',
-        style: TextStyle(
+        'Nutty PWA',
+        style: GoogleFonts.oxygen(
+            textStyle: TextStyle(
           fontSize: 30,
-        ),
+          color: Colors.purple.shade700,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold,
+        )),
       );
 
   Container buildContainer() {
